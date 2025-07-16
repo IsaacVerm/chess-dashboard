1a6441fd9e3c37b6d4c5074b21fffda58f649790
IsaacVerm <isaacverm@gmail.com>
2025-07-16 12:56:10 +0200

    keep metrics calculation by day instead of overwriting

--- calculate_metrics.sh
+++ calculate_metrics.sh
@@ -2,6 +2,7 @@
 
 # Calculate metrics
 # Usage: ./calculate_metrics.sh
+# This assumes there's already a file data/metrics.csv containing the header with fields in the same order as the output results
 
 awk -F',' '
 BEGIN {
@@ -34,10 +35,9 @@ END {
     score_percentage_before_last_week = (max_score_before_last_week > 0) ? score_before_last_week / max_score_before_last_week : 0
     
     # Output results
-    printf "score_last_week,max_score_last_week,score_before_last_week,max_score_before_last_week,score_percentage_last_week,score_percentage_before_last_week,date\n"
     printf "%.1f,%d,%.1f,%d,%.3f,%.3f,%s\n", 
            score_last_week, max_score_last_week, 
            score_before_last_week, max_score_before_last_week,
            score_percentage_last_week, score_percentage_before_last_week,
            current_date
-}' data/games.csv > data/metrics.csv
+}' data/games.csv >> data/metrics.csv
